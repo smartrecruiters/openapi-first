b15250d3c49d001855301c42a8567cfd6f9e7592
Anna Lesniak <a.lesniak@smartrecruiters.com>
2019-05-29 15:04:20 +0200

    Introduce optional realm property in WWW-Authenticate header

--- test/middlewares/error-handlers/missing-scopes.spec.js
+++ test/middlewares/error-handlers/missing-scopes.spec.js
@@ -1,11 +1,12 @@
-const missingScopesHandler = require('../../../middlewares/error-handlers/missing-scopes')()
+const missingScopesHandler = require('../../../middlewares/error-handlers/missing-scopes')
 const MissingRequiredScopes = require('../../../errors/MissingRequiredScopes')
 
 describe('missing scopes error handler', () => {
 
-    it('should catch MissingRequiredScopes', () => {
-        // given
-        const res = {
+    let res
+
+    beforeEach(() => {
+        res = {
             setHeader(name, value) {
                 if (!this.headers) {
                     this.headers = {}
@@ -22,17 +23,34 @@ describe('missing scopes error handler', () => {
                 return this
             }
         }
+    })
+
+    it('should catch MissingRequiredScopes', () => {
+        // given
 
         // when
-        missingScopesHandler(new MissingRequiredScopes(['write', 'delete']), undefined, res, undefined)
+        missingScopesHandler()(new MissingRequiredScopes(['write', 'delete']), undefined, res, undefined)
 
         // then
         expect(res.statusCode).to.eql(403)
         expect(res.headers).to.eql({'WWW-Authenticate':
-                'Bearer scope="write delete", '
-                + 'error="insufficient_scope", '
+                'Bearer scope="write delete", error="insufficient_scope", '
                 + 'error_description="Insufficient scope for this resource"'})
         expect(res.message).to.eql('Missing required scopes: write,delete')
+    })
+
+    it('should catch MissingRequiredScopes and return response header with the given realm', () => {
+        // given
+
+        // when
+        missingScopesHandler({realm: 'Test'})(new MissingRequiredScopes(['write']), undefined, res, undefined)
+
+        // then
+        expect(res.statusCode).to.eql(403)
+        expect(res.headers).to.eql({'WWW-Authenticate':
+                'Bearer realm="Test", scope="write", error="insufficient_scope", '
+                + 'error_description="Insufficient scope for this resource"'})
+        expect(res.message).to.eql('Missing required scopes: write')
     })
 
     it('should not catch different errors', () => {
@@ -40,7 +58,7 @@ describe('missing scopes error handler', () => {
         const next = sinon.spy()
 
         // when
-        missingScopesHandler(new Error(), undefined, undefined, next)
+        missingScopesHandler()(new Error(), undefined, undefined, next)
 
         // then
         sinon.assert.called(next)
